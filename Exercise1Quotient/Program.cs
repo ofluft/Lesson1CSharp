4d524b76c35b00f05f6e7be22779bf44b6cfb032
Дмитрий Степанов <ofluft@gmail.com>
2024-10-03 09:50:39 +1000

    Добавьте файлы проекта.

---
+++ Exercise1Quotient/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercise1Quotient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число:");
+            var a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            var b = Convert.ToInt32(Console.ReadLine());
+
+            if (b == 0)
+            {
+                Console.WriteLine("На ноль делить нельзя");
+                return;
+            }
+
+            var quotient = a / b;
+            var remainder = a % b;
+
+            Console.WriteLine("Результат:\n{0}/ {1} = {2}. Остаток = {3}", a, b, quotient, remainder);
+
+        }
+    }
+}
