7c73f18e3a0b49ce42b0c8cb0d9a56595e38e7ef
Дмитрий Степанов <ofluft@gmail.com>
2024-10-03 09:57:44 +1000

    Добавление проекта

---
+++ Exercise2ArrayCopy/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Exercise2ArrayCopy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var strings = new string[10];
+            for (int i = 0; i < strings.Length; i++)
+                strings[i] = "Элемент" + (i + 1);
+
+            var stringsCopy = new string[strings.Length];
+            for (int i = 0; i < strings.Length; i++)
+                stringsCopy[i] = strings[i];
+
+            Console.WriteLine("Исходный массив:\n" + string.Join(" ", strings));
+            Console.WriteLine("Массив копия:\n" + string.Join(" ", stringsCopy));
+
+        }
+    }
+}
