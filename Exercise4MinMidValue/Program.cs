a85fcbdf1ce9e4787acfa2b7b645a178c3435661
Дмитрий Степанов <ofluft@gmail.com>
2024-10-03 18:10:01 +1000

    Помещение задачи Минимальное и среднее значение массива

---
+++ Exercise4MinMidValue/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercise4MinMidValue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[10];
+            Random random = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+                numbers[i] = random.Next(1, 101);
+
+            var sum = 0;
+            var minValue = numbers[0];
+            foreach (var i in numbers)
+            {
+                sum += i;
+                if (minValue > i)
+                    minValue = i;
+            }
+            Console.WriteLine("Массив значений: " + string.Join(" ", numbers)); 
+            Console.WriteLine("Минимальное значение в массиве: {0}\nСреднее арифметическое значение в массиве: {1}", minValue, sum/ numbers.Length);
+            
+        }
+    }
+}
