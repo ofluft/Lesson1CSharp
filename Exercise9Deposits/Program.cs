bd1a35147fb6494a0a73e55d1223b58d3d3b0a5e
Дмитрий Степанов <ofluft@gmail.com>
2024-10-04 09:20:06 +1000

    Помещение 9 задачи - вклады.

---
+++ Exercise9Deposits/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection.Metadata;
+
+namespace Exercise9Deposits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Укажите размер вклада (рублей):");
+            var startDeposit = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Укажите срок вклада (количество месяцев):");
+            var depositTerm = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Расчет по вкладу:");
+            int monthCounter = 1;
+            CalculateGrowth(startDeposit, monthCounter, depositTerm);
+
+        }
+
+        private static void CalculateGrowth(double deposit, int monthCount, int totalTerm)
+        {
+            if (monthCount > totalTerm)
+            {
+                return;
+            }
+
+            var depositOfTheMonth = Math.Round((double) deposit * 1.02, 4);
+            var growthAmount = Math.Round(depositOfTheMonth - deposit, 4);
+            Console.WriteLine("{0} месяц:\nСумма: {1}\nПрирост за месяц: {2}\n", monthCount, depositOfTheMonth, growthAmount);           
+
+            monthCount++;
+
+            CalculateGrowth(depositOfTheMonth, monthCount, totalTerm);
+
+        }
+
+    }
+}
